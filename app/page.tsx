3c2e7fd131a9c42829ba008a756a4a5df529d03e
Ben Soyka <hello@bsoyka.me>
2026-02-14 17:23:50 -0600

    feat: design landing page (#13)
    * feat: add landing copy
    
    * feat: refactor landing and add sign-in section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import NavBar from "./components/NavBar";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-        <NavBar />
-    </main>
+    <>
+      <NavBar />
+
+      <header className="flex flex-col items-center justify-center text-align-center max-w-3xl m-auto p-10">
+        <h1 className="text-3xl font-bold">
+          Give your friends a <i>Nudge</i>!
+        </h1>
+        <div className="text-lg">
+          Nudge is a collaborative habit tracking platform that keeps you
+          accountable with help from your friends. Start tracking your habits
+          today!
+        </div>
+      </header>
+
+      <main className="flex flex-col items-center justify-center text-align-center max-w-3xl m-auto p-10">
+        <h3 className="text-xl font-bold">Sign up or log in</h3>
+        <button>Sign in with Google</button>
+      </main>
+    </>
   );
 }
