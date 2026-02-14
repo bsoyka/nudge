305a7ec699cc851283f75e75e7cd328bab313d24
steph971 <steph971@umn.edu>
2026-02-14 12:53:39 -0600

    Removed some css

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,12 @@
 'use client';
 import Link from "next/link"
 
-
-
 export default function() {
 	
-	return( <>
+	return( <div>
 		<Link href= {{pathname: "/"}}> Home </Link>
 		<Link href= {{pathname: "/page1"}}> Other </Link>
-	</>
+	</div>
 	);
 
 }
