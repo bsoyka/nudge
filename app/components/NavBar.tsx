0aa0ea333a04ee3ea1e8d355211f8bd03e32e07f
Ben Soyka <hello@bsoyka.me>
2026-02-14 16:57:17 -0600

    feat: add basic logo

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,6 @@
 //'use client';
 import Link from "next/link";
+import Image from "next/image";
 
 function NavBarLink({ path, name }: { path: string; name: string }) {
   return (
@@ -12,6 +13,15 @@ function NavBarLink({ path, name }: { path: string; name: string }) {
 function NavBar() {
   return (
     <nav className="flex w-full h-14 gap-3 justify-start items-center bg-primary p-5">
+      <Link href="/" className="flex items-center h-full mr-4">
+        <Image
+          src="/nudge-logo.svg"
+          alt="Nudge logo"
+          width={120}
+          height={40}
+          className="h-full w-auto"
+        />
+      </Link>
       <NavBarLink path="/" name="Dashboard" />
       <NavBarLink path="/page1" name="Other Page" />
     </nav>
