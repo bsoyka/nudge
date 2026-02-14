e9c032d0558246bc651a00ba66c84e01d16ca901
Ben Soyka <hello@bsoyka.me>
2026-02-14 15:22:55 -0600

    feat: add landing copy

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-        <NavBar />
-    </main>
+    <>
+      <NavBar />
+
+      <main
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <h1>
+          Give your friends a <i>Nudge</i>!
+        </h1>
+        <p>
+          Nudge is a collaborative habit tracking platform that keeps you
+          accountable with help from your friends. Start tracking your habits
+          today!
+        </p>
+      </main>
+    </>
   );
 }
