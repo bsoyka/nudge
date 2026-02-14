f33a7fce0504e0110d58cd7ac12ef37b86d74d2b
Ben Soyka <hello@bsoyka.me>
2026-02-14 17:47:36 -0600

    refactor: clean up branding, use Tailwind classes, refactor navbar (#16)
    * refactor: use Tailwind classes, clean up nav bar, set up colors
    
    * fix: address review comments
    
    * feat: add basic logo
    
    * chore: remove unused SVGs

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,31 @@
 //'use client';
-import Link from "next/link"
-import "../styles/nav-bar.css";
+import Link from "next/link";
+import Image from "next/image";
 
-function NavBar() {
-	return( <div className="nav-bar">
-		<Link className="page-link" href= {{pathname: "/"}}> Home </Link>
-		<Link className="page-link" href= {{pathname: "/dashboard"}}> Other Page </Link>
-	</div>
-	);
+function NavBarLink({ path, name }: { path: string; name: string }) {
+  return (
+    <Link className="border-b-2 border-white" href={{ pathname: path }}>
+      {name}
+    </Link>
+  );
 }
 
+function NavBar() {
+  return (
+    <nav className="flex w-full h-14 gap-3 justify-start items-center bg-primary p-5">
+      <Link href="/" className="flex items-center h-full mr-4">
+        <Image
+          src="/nudge-logo.svg"
+          alt="Nudge logo"
+          width={120}
+          height={40}
+          className="h-full w-auto"
+        />
+      </Link>
+      <NavBarLink path="/dashboard" name="Dashboard" />
+      <NavBarLink path="/leaderboard" name="Leaderboard" />
+    </nav>
+  );
+}
 
 export default NavBar;
