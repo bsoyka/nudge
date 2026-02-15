3df675ffaff58d4e714bfe869924e3cfe4bb44d6
Ben Soyka <hello@bsoyka.me>
2026-02-15 06:44:42 -0600

    fix: add social to navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -26,6 +26,7 @@ function NavBar() {
         <NavBarLink path="/dashboard" name="Dashboard" />
         <NavBarLink path="/leaderboard" name="Leaderboard" />
         <NavBarLink path="/searchbar" name="Search" />
+        <NavBarLink path="/social" name="Social" />
         <NavBarLink path="/verify" name="Verify for Friends" />
       </div>
     </nav>
