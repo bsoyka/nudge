0705e3f1936e68588e99183f22f5959f3fc32335
Aidan DuMond <aidan.dumond@gmail.com>
2026-02-15 07:01:23 -0600

    get friend req update

--- firebase/get-friend-requests.ts
+++ firebase/get-friend-requests.ts
@@ -1,8 +1,9 @@
 import { getAuth } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "./auth";
+import { Friend } from "@/app/constants";
 
-export const getFriendRequests = async (): Promise<string[] | undefined> => {
+export const getFriendRequests = async () : Promise<Friend[] | undefined> => {
   // get current user
   const auth = getAuth();
   const user = auth.currentUser;
@@ -13,14 +14,39 @@ export const getFriendRequests = async (): Promise<string[] | undefined> => {
   }
 
   try {
+    let friendReqList : Friend[] = [];
+
     const userRef = doc(db, "users", user.uid);
     const userSnap = await getDoc(userRef);
 
     if (userSnap.exists()) {
-      return userSnap.data().friendRequests;
+      const userData : string[] = userSnap.data().friendRequests
+
+      for (const friendUid of userData) {
+        const friendRef = doc(db, "users", friendUid);
+        const friendSnap = await getDoc(friendRef);
+
+        if (friendSnap.exists()) {
+          const friendData = friendSnap.data();
+
+          const friendItem: Friend = {
+            uid: friendSnap.id,
+            username: friendData.username,
+            score: friendData.score,
+            photo: friendData.photo ?? null
+          };
+
+          friendReqList.push(friendItem)
+
+        }
+      }
+
     } else {
       throw new Error("user does not exist");
     }
+
+    return friendReqList
+
   } catch (error: any) {
     console.error(error.code, error.message);
   }
