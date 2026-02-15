e647b0ae3cd8f88f73739a3ea4a1f74166dd754f
Ben Soyka <hello@bsoyka.me>
2026-02-15 04:37:45 -0600

    style: auto format components

--- app/components/HabitBox.tsx
+++ app/components/HabitBox.tsx
@@ -1,31 +1,33 @@
 "use client";
 import "../styles/habitbox.css";
-import {Habit, Status} from "../constants"
-import { Check, X, CircleQuestionMark} from "lucide-react";
+import { Habit, Status } from "../constants";
+import { Check, X, CircleQuestionMark } from "lucide-react";
 import { useState } from "react";
 
-interface HabitBoxProps{
-	habit : Habit,
+interface HabitBoxProps {
+  habit: Habit;
 }
 
-function HabitBox(habitProp : HabitBoxProps){
-	const [getStatus, setStatus] = useState(habitProp.habit.status);
-	const handlePendingRequest = () =>
-	{
-		setStatus(Status.PENDING);
-		//API CALL
-	}
-	return(
-		<div className="habit-box">
-			<h1 className="habit-name"> {habitProp.habit.name}</h1>
-			<h3 className="streak-text">Streak: {habitProp.habit.streak}</h3>
-			<h3 className="streak-text">Auditor: {habitProp.habit.auditor}</h3>
-			{getStatus == Status.FINISHED ? <Check id="check" className="status-box"/> 
-				: getStatus == Status.PENDING 
-					? <CircleQuestionMark id={"question-mark"} className="status-box" /> 
-				: <X id="x" className="status-box" onClick={handlePendingRequest}/>}
-		</div>
-	);
+function HabitBox(habitProp: HabitBoxProps) {
+  const [getStatus, setStatus] = useState(habitProp.habit.status);
+  const handlePendingRequest = () => {
+    setStatus(Status.PENDING);
+    //API CALL
+  };
+  return (
+    <div className="habit-box">
+      <h1 className="habit-name"> {habitProp.habit.name}</h1>
+      <h3 className="streak-text">Streak: {habitProp.habit.streak}</h3>
+      <h3 className="streak-text">Auditor: {habitProp.habit.auditor}</h3>
+      {getStatus == Status.FINISHED ? (
+        <Check id="check" className="status-box" />
+      ) : getStatus == Status.PENDING ? (
+        <CircleQuestionMark id={"question-mark"} className="status-box" />
+      ) : (
+        <X id="x" className="status-box" onClick={handlePendingRequest} />
+      )}
+    </div>
+  );
 }
 
 export default HabitBox;
--- app/components/Searchbar.jsx
+++ app/components/Searchbar.jsx
@@ -1,55 +1,53 @@
-"use client"
-import React, {useState} from 'react';
+"use client";
+import React, { useState } from "react";
 
-
-import {FaSearch} from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 
-export const SearchBar = ({setResults}) => {
-    const [input, setInput] = useState("");
+export const SearchBar = ({ setResults }) => {
+  const [input, setInput] = useState("");
 
-    const fetchData = async (value) => {
-        if(!value) return;
-        const q = 
-        query(
-        collection(db, "users"), 
-        where("name", ">=", value),
-        where("name", "<=", value + "\uf8ff")
-        );
+  const fetchData = async (value) => {
+    if (!value) return;
+    const q = query(
+      collection(db, "users"),
+      where("name", ">=", value),
+      where("name", "<=", value + "\uf8ff"),
+    );
 
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach(doc => ({
-            id: doc.id,
-            ...doc.data()
-        }));
-        setResults(results);
-        /*
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setResults(results);
+    /*
         fetch("put call here").then((response) => response.json())
         .then((json) => {
             const results = json.filter((user) => {
                 return(
-                value && 
+                value &&
                 user &&
-                user.name && 
+                user.name &&
                 user.name.toLowerCase().includes(value)
                 )
         });
         */
-       //https://firebase.google.com/docs/firestore/query-data/get-data#web_1
-       //changed to firebase instead of api call
-    
-
-    }
-    const handleChange = (value) =>{
-        setInput(value);
-        fetchData(value);
-    }
-    return(
-        <div className="input-wrapper">
-            <FaSearch id="search-icon" />
-            <input placeholder = "Type to search.." value={input}
-             onChange={(e) => setInput(e.target.value)} />
-        </div>
-    )
-
-}
+    //https://firebase.google.com/docs/firestore/query-data/get-data#web_1
+    //changed to firebase instead of api call
+  };
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type to search.."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </div>
+  );
+};
