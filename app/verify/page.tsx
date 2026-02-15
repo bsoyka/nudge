1614c09a7b74f286281a3b3b41af3e6dc08027f4
Ben Soyka <hello@bsoyka.me>
2026-02-15 03:59:01 -0600

    style: format

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -5,12 +5,7 @@ import { Habit, Status } from "../constants";
 import Footer from "../components/Footer";
 import PendingBoxes from "./PendingBoxes";
 
-
-
-
 export default function VerifyPage() {
-
-
   return (
     <div>
       <NavBar />
@@ -19,10 +14,12 @@ export default function VerifyPage() {
         <h1 className="text-2xl font-bold">Verify Your Friends&apos; Habits</h1>
 
         <p className="text-lg mt-4">
-              Your friends have been productive, and they’re counting on you to keep them honest. Verify whether they’ve actually completed their daily habits. Not sure? Reach out to nudge them back on track.
-            </p>
+          Your friends have been productive, and they’re counting on you to keep
+          them honest. Verify whether they’ve actually completed their daily
+          habits. Not sure? Reach out to nudge them back on track.
+        </p>
 
-            <PendingBoxes/>
+        <PendingBoxes />
       </main>
 
       <Footer />
