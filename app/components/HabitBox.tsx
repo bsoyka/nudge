3e520c909ca2586cfb50ec3916f4b940cd78a446
steph971 <steph971@umn.edu>
2026-02-15 06:41:22 -0600

    asdfasdf

--- app/components/HabitBox.tsx
+++ app/components/HabitBox.tsx
@@ -1,8 +1,9 @@
 "use client";
-import "../styles/habitbox.css";
+
 import "../styles/checkbox.css";
+import "../styles/habitbox.css";
 import { Habit, Status } from "../constants";
-import { Check, X, CircleQuestionMark } from "lucide-react";
+import { Check, SquareDashed, CircleQuestionMark } from "lucide-react";
 import { useState } from "react";
 
 interface HabitBoxProps {
@@ -18,14 +19,14 @@ function HabitBox(habitProp: HabitBoxProps) {
   return (
     <div className="habit-box">
       <h1 className="habit-name"> {habitProp.habit.habitName}</h1>
-      <h3 className="streak-text">Streak: {habitProp.habit.streak}</h3>
+      <h3 className="streak-text">Streak: {habitProp.habit.streak} 🔥</h3>
       <h3 className="streak-text">Auditor: {habitProp.habit.auditor}</h3>
       {getStatus == Status.FINISHED ? (
         <Check id="check" className="status-box" />
       ) : getStatus == Status.PENDING ? (
         <CircleQuestionMark id={"question-mark"} className="status-box" />
       ) : (
-        <X id="x" className="status-box" onClick={handlePendingRequest} />
+        <SquareDashed id="square" className="status-box" onClick={handlePendingRequest} />
       )}
     </div>
   );
--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -6,6 +6,8 @@ import HabitBox from "../components/HabitBox";
 import { Habit } from "../constants";
 import "../styles/dashboard.css";
 import "../globals.css";
+import FriendRequestArea from "../social/FriendRequestArea";
+import PendingBoxes from "../verify/PendingBoxes";
 
 function Dashboard(){
 	let [getUserHabits, setUserHabits] = useState<Habit[]>([]);
@@ -24,11 +26,18 @@ function Dashboard(){
 
 	return(
 	<div className="dashboard">
+			<div className="habits">
 			{getUserHabits.map((habit : Habit) => <HabitBox habit={habit} key={habit.hid} />)}
 			<ModalTriggerButton getHabits={getUserHabits} setHabits={setUserHabits}/>
+			</div>
+			<div className="friends">
+				<FriendRequestArea/>
+			</div>
+			<div className="pending">
+			<PendingBoxes/>
+			</div>
 	</div>
 	);
-
 };
 
 export default Dashboard;
