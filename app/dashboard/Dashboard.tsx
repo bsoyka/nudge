0771d7ccbe48f8b0e944cb2a7aa2ddb7d559425b
steph971 <steph971@umn.edu>
2026-02-15 07:27:18 -0600

    youve got mail:

--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
 "use client";
+import Link from "next/link";
 import ModalTriggerButton from "./ModalTriggerButton";
 import { getHabits } from "@/firebase/get-habits";
+import { getPendingHabits } from "@/firebase/get-pending-habits";
 import { useEffect, useState } from "react";
 import HabitBox from "../components/HabitBox";
 import { Habit } from "../constants";
@@ -23,7 +25,27 @@ function Dashboard(){
 		}
 		fetchHabits();
 	}, []); 
+	let [getNumPending, setNumPending] = useState<number>();
+	useEffect(() => {
+		const fetchPending = async () => {
+			const promise = await getPendingHabits();
+			if(promise == null){
+				setNumPending(0);
+			}
+			else{
+				setNumPending(_ => promise);
+			}
+		}
+		fetchPending();
+	}, []); 
 
+	function GoToPending(){
+		return(
+			<div className="pending">
+				<Link href="/verify"> You have {getNumPending} friend habits to checkout out! </Link>
+			</div>
+		);
+	};
 	return(
 	<div className="dashboard">
 			<div className="habits">
@@ -33,9 +55,7 @@ function Dashboard(){
 			<div className="friends">
 				<FriendRequestArea/>
 			</div>
-			<div className="pending">
-			<PendingBoxes/>
-			</div>
+			<GoToPending/>
 	</div>
 	);
 };
