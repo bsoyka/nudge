0b8d176d3bb5ccb12c6de69f973790c93c9f9368
Ben Soyka <hello@bsoyka.me>
2026-02-14 20:07:52 -0600

    refactor: allow other props to pass to Button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,17 +1,17 @@
+import React from "react";
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
 export default function Button({
   children,
   className,
-  onClick,
-}: {
-  children: React.ReactNode;
-  className?: string;
-  onClick?: () => void;
-}) {
+  type = "button",
+  ...rest
+}: ButtonProps) {
+  const combinedClassName = `bg-secondary text-white px-4 py-2 rounded-md hover:bg-dark cursor-pointer ${className || ""}`;
+
   return (
-    <button
-      className={`bg-secondary text-white px-4 py-2 rounded-md hover:bg-secondary-dark cursor-pointer ${className || ""}`}
-      onClick={onClick}
-    >
+    <button type={type} className={combinedClassName} {...rest}>
       {children}
     </button>
   );
