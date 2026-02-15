3851b5228632bcff1b4fb0a3e402986201266684
Aidan DuMond <aidan.dumond@gmail.com>
2026-02-15 06:31:46 -0600

    added friend request button

---
+++ app/components/SearchCard.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import { SearchUser } from "../constants";
+
+type SearchCardProps = {
+  username: string;
+  photo: string | null
+}
+
+export default function SearchCard({username, photo}: SearchCardProps) {
+  const IMAGE_SIZE = 40;
+  return (
+    <div className="flex flex-row border-black border-2 w-100 p-5 rounded-3xl items-center justify-between">
+      <div className="flex flex-row justify-center">
+        <div>
+          {photo ? (
+            <Image
+              src={photo}
+              alt={"user"}
+              height={IMAGE_SIZE}
+              width={IMAGE_SIZE}
+              className="rounded-full"
+            />
+          ) : (
+            <div
+              className={`w-[${IMAGE_SIZE}px] h-[${IMAGE_SIZE}px] rounded-full bg-gray-300 flex items-center justify-center`}
+            >
+              {username?.charAt(0)}
+            </div>
+          )}
+        </div>
+        <div className="text-2xl pl-3 text-center">
+          {username}
+        </div>
+      </div>
+        <div className="">
+          Add Friend
+        </div>
+    </div>
+
+  );
+}
